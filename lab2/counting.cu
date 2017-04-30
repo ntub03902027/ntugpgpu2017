47d915db040197e82f88a4774d08595441ec6aa5
ntub03902027 <b03902027@ntu.edu.tw>
2017-04-30 23:02:18 +0800

    finished Part I

--- lab2/counting.cu
+++ lab2/counting.cu
@@ -7,11 +7,25 @@
 #include <thrust/device_ptr.h>
 #include <thrust/execution_policy.h>
 
+#include <thrust/replace.h>
+
 __device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
 __device__ __host__ int CeilAlign(int a, int b) { return CeilDiv(a, b) * b; }
 
+
+struct is_alphabet{
+	__host__ __device__ 
+	bool operator()(int x) {
+		return x >= (int)'a' && x <= (int)'z';
+	}
+};
+
+
 void CountPosition1(const char *text, int *pos, int text_size)
 {
+		thrust::replace_copy(thrust::device, text, text + text_size, pos, (int)'\n', 0);
+		thrust::replace_if(thrust::device, pos, pos + text_size, is_alphabet(), 1);
+		thrust::inclusive_scan_by_key(thrust::device, pos, pos + text_size, pos, pos);
 }
 
 void CountPosition2(const char *text, int *pos, int text_size)
