8af5be67af46e7fbd785e6dc925111ebcb5ab273
ntub03902027 <b03902027@ntu.edu.tw>
2017-05-01 22:02:11 +0800

    Part II: a naïve O(nk) method

--- lab2/counting.cu
+++ lab2/counting.cu
@@ -8,6 +8,8 @@
 #include <thrust/execution_policy.h>
 
 #include <thrust/replace.h>
+#define nThreads 1024
+//#define BinaryIndexTree
 
 __device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
 __device__ __host__ int CeilAlign(int a, int b) { return CeilDiv(a, b) * b; }
@@ -20,14 +22,27 @@ struct is_alphabet{
 	}
 };
 
+__global__ void fillNaive(const char *text, int *pos) {
+	const int x = blockIdx.x * blockDim.x + threadIdx.x;
+	int i = x;
+	while (i >= 0 && text[i] != '\n') {
+		i--;
+	}
+	pos[x] = x - i;
+}
 
 void CountPosition1(const char *text, int *pos, int text_size)
 {
-		thrust::replace_copy(thrust::device, text, text + text_size, pos, (int)'\n', 0);
-		thrust::replace_if(thrust::device, pos, pos + text_size, is_alphabet(), 1);
-		thrust::inclusive_scan_by_key(thrust::device, pos, pos + text_size, pos, pos);
+	thrust::replace_copy(thrust::device, text, text + text_size, pos, (int)'\n', 0);
+	thrust::replace_if(thrust::device, pos, pos + text_size, is_alphabet(), 1);
+	thrust::inclusive_scan_by_key(thrust::device, pos, pos + text_size, pos, pos);
 }
 
 void CountPosition2(const char *text, int *pos, int text_size)
 {
+
+#ifndef BinaryIndexTree
+	fillNaive<<<(text_size-1)/nThreads + 1, nThreads>>>(text, pos);
+#else
+#endif
 }
